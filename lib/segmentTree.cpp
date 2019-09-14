e67b49e873a588a49f641f47c0bf143ec96e64b1
masutech16 <leafclover64@gmail.com>
2019-09-14 15:41:40 +0900

    update segment tree

--- lib/segmentTree.cpp
+++ lib/segmentTree.cpp
@@ -7,67 +7,86 @@ const ll MOD = 1000000007;
 
 using namespace std;
 
-class SegmentTree {
+template <class T> class SegmentTree {
 public:
-  SegmentTree(const int n, const int identity);
-  void update(const int index, const int value);
-  int find(const int l, const int r);
+  SegmentTree(const int n, const T id, function<T(T, T)> op,
+              function<T(T, T)> updateOp);
+  void update(const int index, const T value);
+  T find(const int l, const int r);
 
 private:
   int leftChildIndex(const int parent);
   int rightChildIndex(const int parent);
   int parent(const int child);
   int leaf(const int index);
-  int query(const int l, const int r, const int node, const int al,
-            const int ar);
+  T query(const int l, const int r, const int node, const int al, const int ar);
 
 private:
-  vector<int> array;
-  int size;
-  int id;
+  vector<T> data;
+  int leafNum;
+  T id;
+  function<T(T, T)> op;
+  function<T(T, T)> updateOp;
 };
 
-SegmentTree::SegmentTree(const int n, const int identity) {
-  id = identity;
-  size = 1;
-  while (size < n)
-    size *= 2;
-  array = vector<int>(2 * size - 1, id);
+template <class T>
+SegmentTree<T>::SegmentTree(const int n, const T id, function<T(T, T)> op,
+                            function<T(T, T)> updateOp)
+    : id(id), op(op), updateOp(updateOp) {
+  leafNum = 1;
+  while (leafNum < n)
+    leafNum *= 2;
+  data = vector<T>(2 * leafNum - 1, id);
 }
 
-int SegmentTree::leftChildIndex(const int parent) { return parent * 2 + 1; }
-int SegmentTree::rightChildIndex(const int parent) { return parent * 2 + 2; }
-int SegmentTree::parent(const int child) { return (child - 1) / 2; }
-int SegmentTree::leaf(const int index) { return index + size - 1; }
-int SegmentTree::find(const int l, const int r) {
-  return query(l, r, 0, 0, size);
+template <class T> int SegmentTree<T>::leftChildIndex(const int parent) {
+  return parent * 2 + 1;
 }
 
-int SegmentTree::query(const int l, const int r, const int node, const int al,
-                       const int ar) {
+template <class T> int SegmentTree<T>::rightChildIndex(const int parent) {
+  return parent * 2 + 2;
+}
+
+template <class T> int SegmentTree<T>::parent(const int child) {
+  return (child - 1) / 2;
+}
+
+template <class T> int SegmentTree<T>::leaf(const int index) {
+  return index + leafNum - 1;
+}
+
+template <class T> T SegmentTree<T>::find(const int l, const int r) {
+  return query(l, r, 0, 0, leafNum);
+}
+
+template <class T>
+T SegmentTree<T>::query(const int l, const int r, const int node, const int al,
+                        const int ar) {
   if (r <= al || ar <= l)
     return id;
   if (l <= al && ar <= r)
-    return array[node];
+    return data[node];
   int mid = (al + ar) / 2;
   int leftValue = query(l, r, leftChildIndex(node), al, mid);
   int rightValue = query(l, r, rightChildIndex(node), mid, ar);
-  return min(leftValue, rightValue);
+  return op(leftValue, rightValue);
 }
 
-void SegmentTree::update(const int index, const int value) {
+template <class T> void SegmentTree<T>::update(const int index, const T value) {
   int i = leaf(index);
-  array[i] = value;
+  data[i] = updateOp(data[i], value);
   while (i > 0) {
     i = parent(i);
-    array[i] = min(array[rightChildIndex(i)], array[leftChildIndex(i)]);
+    data[i] = op(data[rightChildIndex(i)], data[leftChildIndex(i)]);
   }
 }
 
 int main() {
   int n, q;
   cin >> n >> q;
-  SegmentTree seg = SegmentTree(n, 2147483647);
+  SegmentTree<ll> seg(n, (1LL << 31) - 1,
+                      [](ll a, ll b) -> ll { return min(a, b); },
+                      [](ll a, ll b) -> ll { return b; });
   REP(i, q) {
     int com, x, y;
     cin >> com >> x >> y;
