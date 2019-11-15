3f5f2d53b16205a916d1f04fdc0c4496e658d988
masutech16 <leafclover64@gmail.com>
2019-11-15 12:37:26 +0900

    update dijkstra

--- lib/dijkstra.cpp
+++ lib/dijkstra.cpp
@@ -4,38 +4,33 @@
 #define FOR(i, n, m) for (int i = n; i < m; i++)
 #define ll long long int
 
-struct edge
-{
+using namespace std;
+
+struct edge {
   int to, cost;
 };
 typedef vector<vector<edge>> graph;
-const ll MOD = 1000000007;
-using namespace std;
 
-int d[100000] = {};
-
-void dijkstra(int s, int n, graph g)
-{
+void dijkstra(int s, int n, graph &g, vector<int> &d) {
   // (最短距離, 頂点)
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+  priority_queue<pair<int, int>, vector<pair<int, int>>,
+                 greater<pair<int, int>>>
+      q;
 
-  fill(d, d + n, INT_MAX);
+  REP(i, n) { d[i] = INT_MAX; }
   d[s] = 0;
   q.push(make_pair(0, s));
 
-  while (!q.empty())
-  {
+  while (!q.empty()) {
     auto p = q.top();
     q.pop();
 
     int v = p.second;
     if (d[v] < p.first)
       continue;
-    REP(i, g[v].size())
-    {
+    REP(i, g[v].size()) {
       auto e = g[v][i];
-      if (d[e.to] > d[v] + e.cost)
-      {
+      if (d[e.to] > d[v] + e.cost) {
         d[e.to] = d[v] + e.cost;
         q.push(make_pair(d[e.to], e.to));
       }
