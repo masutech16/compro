a4cbad90f958da0fb4b2cd372f1ff24eb723edc8
masutech16 <leafclover64@gmail.com>
2020-03-21 20:53:57 +0900

    change config

--- acc/submit.sh
+++ acc/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 contest=$(basename `pwd`)
 cd $1
-oj-bundle template.cpp > submit.cpp
+oj-bundle "${contest}_$1.cpp" > submit.cpp
 oj s "https://atcoder.jp/contests/${contest}/tasks/${contest}_$1" submit.cpp --no-open -y
