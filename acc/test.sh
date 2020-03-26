bbcf38eb5cb9b2dbec4918505f0c68f1e56d3b70
masutech16 <leafclover64@gmail.com>
2020-03-26 13:51:08 +0900

    update shell script

--- acc/test.sh
+++ acc/test.sh
@@ -2,4 +2,4 @@
 contest=$(basename `pwd`)
 basename `pwd`
 cd $1
-g++ -std=c++14 -Wall -Wextra -fuse-ld=gold -fsanitize=undefined "${contest}_${1}.cpp" && oj test
+g++ -std=c++14 "-I${COMPRO_LIB}" -Wall -Wextra -fuse-ld=gold -fsanitize=undefined "${contest}_${1}.cpp" && oj test
