90b562dab635423accbc5d7de26bb0782ed05bcc
masutech16 <leafclover64@gmail.com>
2020-04-12 17:20:49 +0900

    Update g++ compiler version

--- acc/test.sh
+++ acc/test.sh
@@ -2,4 +2,4 @@
 contest=$(basename `pwd`)
 basename `pwd`
 cd $1
-g++ -std=c++14 "-I${COMPRO_LIB}" -Wall -Wextra -fuse-ld=gold -fsanitize=undefined "${contest}_${1}.cpp" && oj test
+g++-9 -std=c++17 "-I${COMPRO_LIB}" -Wall -Wextra -fuse-ld=gold -fsanitize=undefined "${contest}_${1}.cpp" && oj test
