6a45dffa9da52f506d158b13d76d31ad2c936212
masutech16 <leafclover64@gmail.com>
2020-07-27 15:13:34 +0000

    Add setup shell script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# set alias
+#
+alias oj-submit='oj s -y --open'
+alias g++='g++-8 -std=c++17 -I /workspaces/compro/lib -Wall -Wextra -fuse-ld=gold -fsanitize=undefined'
+alias oj-bundle='oj-bundle -I /workspaces/compro/lib'
