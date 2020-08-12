c58e6178863c0986c1c9c97779e4edaf87e7c43f
masutech16 <leafclover64@gmail.com>
2020-08-12 12:51:54 +0000

    Update tools

--- workspace/watch.sh
+++ workspace/watch.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 INTERVAL=1 #監視間隔, 秒で指定
-last=`ls -l --full-time tmp.cpp | awk '{print $7}'`
+last=`ls -l --full-time $1 | awk '{print $7}'`
 while true; do
         sleep $INTERVAL
-        current=`ls -l --full-time tmp.cpp | awk '{print $7}'`
+        current=`ls -l --full-time $1 | awk '{print $7}'`
         if test $last != $current ; then
                 echo ""
                 echo "updated: $current"
                 last=$current
-                eval "g++-8 -std=c++17 -I ../../lib/ -Wall -Wextra -fuse-ld=gold -fsanitize=undefined tmp.cpp && oj-bundle -I '../../lib/' tmp.cpp > submit.cpp"
+                eval "g++-8 -std=c++17 -I $SCRIPT_DIR/../lib/ -Wall -Wextra -fuse-ld=gold -fsanitize=undefined $1 && oj-bundle -I '$SCRIPT_DIR/../lib/' $1 > submit.cpp"
         fi
 done
