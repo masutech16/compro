972e033dc198db86deda944e1f115db4728a544b
masutech16 <leafclover64@gmail.com>
2020-12-20 06:41:43 +0000

    Remove unused template

--- lib/template.hpp
+++ lib/template.hpp
@@ -13,10 +13,6 @@
 
 using namespace std;
 
-template <class T> void say(bool val, T yes, T no) { cout << (val ? yes : no) << "\n"; }
-
-void say(bool val, string yes = "Yes", string no = "No") { say<string>(val, yes, no); }
-
 template <class T> void chmin(T &a, T b) {
   if (a > b)
     a = b;
@@ -25,16 +21,6 @@ template <class T> void chmin(T &a, T b) {
 template <class T> void chmax(T &a, T b) {
   if (a < b)
     a = b;
-}
-
-// C++ 17に完全移行したら消す
-// 最大公約数を求める
-template <class T> T gcd(T n, T m) { return n ? gcd(m % n, n) : m; }
-
-// 最小公倍数を求める
-template <class T> T lcm(T n, T m) {
-  int g = gcd(n, m);
-  return n * m / g;
 }
 
 // 重複を消す。計算量はO(NlogN)
