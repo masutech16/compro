e359264c6f6843650f04256ee8f94a8d12532a45
masutech16 <leafclover64@gmail.com>
2020-12-20 06:39:23 +0000

    remove setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-alias oj-submit='oj -c /workspaces/compro/oj/oj-cookie.jar s -y --open'
-alias g++='g++-8 -std=c++17 -I /workspaces/compro/lib -Wall -Wextra -fuse-ld=gold -fsanitize=undefined'
-alias oj-bundle='oj-bundle -I /workspaces/compro/lib'
-alias oj-prepare='oj-prepare --config-file "/workspaces/compro/oj/oj-prepare.config.toml"'
