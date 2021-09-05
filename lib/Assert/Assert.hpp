85de605ff64e250c07a240227404b854af0a48e3
masutech16 <leafclover64@gmail.com>
2021-09-05 06:43:22 +0000

    アサート用のマクロを定義

---
+++ lib/Assert/Assert.hpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#ifndef MY_ASSERT
+#define MY_ASSERT
+
+#ifdef LOCAL
+
+#define ASSERT_MSG(expr, msg)                                                                                          \
+  do {                                                                                                                 \
+    if (!(expr)) {                                                                                                     \
+      std::cerr << msg << std::endl;                                                                                   \
+      assert(expr);                                                                                                    \
+    }                                                                                                                  \
+  } while (0)
+
+#define FAIL ASSERT_MSG(false, "到達しない想定の場所に到達しました")
+
+#else
+
+#define ASSERT_MSG(...) (void)0
+#define FAIL (void)0
+
+#endif
+
+#endif
