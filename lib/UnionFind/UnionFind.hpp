7d3a0658a384ae851fd464e589dc9ed31992a682
masutech16 <leafclover64@gmail.com>
2021-09-05 11:36:41 +0000

    UFのライブラリにassertを仕込んだ

--- lib/UnionFind/UnionFind.hpp
+++ lib/UnionFind/UnionFind.hpp
@@ -1,19 +1,24 @@
-#include <iostream>
 #include <vector>
+
+#include "Assert/Assert.hpp"
+
 #ifndef UNION_FIND
 #define UNION_FIND
-class UnionFind {
-private:
-  std::vector<int> parent;
-  std::vector<int> size;
 
+// UnionFind木です
+// グループのマージ、取得が高速に行えます
+class UnionFind {
 public:
-  UnionFind(const int s) {
+  // s: 管理する集合の要素数
+  UnionFind(int s) : s(s) {
     parent = std::vector<int>(s, -1);
     size = std::vector<int>(s, 1);
   }
 
+  // x,yをマージする
   void merge(int x, int y) {
+    check_range(x);
+    check_range(y);
     int rx = root(x), ry = root(y);
     if (rx == ry)
       return;
@@ -24,14 +29,34 @@ public:
     size[ry] = size[rx];
   }
 
-  int get_size(int x) { return size[root(x)]; }
+  // xが所属しているグループの大きさを返す
+  int get_size(int x) {
+    check_range(x);
+    return size[root(x)];
+  }
 
+  // xが所属しているグループの代表元を返す
   int root(int x) {
+    check_range(x);
     if (parent[x] == -1)
       return x;
     return parent[x] = root(parent[x]);
   }
 
-  bool is_same(int x, int y) { return root(x) == root(y); }
+  // xとyが同じグループに所属しているかを判定する
+  bool is_same(int x, int y) {
+    check_range(x);
+    check_range(y);
+    return root(x) == root(y);
+  }
+
+private:
+  // xがvalidな値かどうかを調べます
+  void check_range(int x) { ASSERT_MSG(0 <= x && x < s, "引数で渡す値は0以上s未満である必要があります"); }
+
+private:
+  const int s;
+  std::vector<int> parent;
+  std::vector<int> size;
 };
 #endif
--- lib/UnionFind/WeightedUnionFind.hpp
+++ lib/UnionFind/WeightedUnionFind.hpp
@@ -1,25 +1,27 @@
-#include <cassert>
-#include <iostream>
 #include <vector>
+
+#include "Assert/Assert.hpp"
+
 #ifndef WEIGHTED_UNION_FIND
 #define WEIGHTED_UNION_FIND
 
 // 重み付きUnionFind木
 // 各ノードに重みを付けることができるUnionFind木。できる操作はUFでできることと同じで、各グループの重みの和が取れます。
 template <class T> class WeightedUnionFind {
-private:
-  std::vector<int> parent;
-  std::vector<int> size;
-  std::vector<T> weight;
-
 public:
-  WeightedUnionFind(const int s, std::vector<T> weight) : weight(weight) {
+  // s: 管理する集合の要素数
+  // weight: 初期の重み。サイズがsである必要がある
+  WeightedUnionFind(const int s, std::vector<T> weight) : s(s), weight(weight) {
+    ASSERT_MSG(s == (int)weight.size(), "UFの管理するサイズと重さ配列の長さは一致している必要があります");
     parent = std::vector<int>(s, -1);
     size = std::vector<int>(s, 1);
   }
 
   // x,yをマージする
   void merge(int x, int y) {
+    check_range(x);
+    check_range(y);
+
     int rx = root(x), ry = root(y);
     if (rx == ry)
       return;
@@ -33,19 +35,40 @@ public:
   }
 
   // xが所属しているグループの大きさを返す
-  int get_size(int x) { return size[root(x)]; }
+  int get_size(int x) {
+    check_range(x);
+    return size[root(x)];
+  }
 
   // xが所属しているグループの重みの総和を返す
-  T get_weight(int x) { return weight[root(x)]; }
+  T get_weight(int x) {
+    check_range(x);
+    return weight[root(x)];
+  }
 
   // xが所属しているグループの代表元を返す
   int root(int x) {
+    check_range(x);
     if (parent[x] == -1)
       return x;
     return parent[x] = root(parent[x]);
   }
 
   // xとyが同じグループに所属しているかを判定する
-  bool is_same(int x, int y) { return root(x) == root(y); }
+  bool is_same(int x, int y) {
+    check_range(x);
+    check_range(y);
+    return root(x) == root(y);
+  }
+
+private:
+  // xがvalidな値かどうかを調べます
+  void check_range(int x) { ASSERT_MSG(0 <= x && x < s, "引数で渡す値は0以上s未満である必要があります"); }
+
+private:
+  const int s;
+  std::vector<int> parent;
+  std::vector<int> size;
+  std::vector<T> weight;
 };
 #endif
